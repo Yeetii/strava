c54f9f0a6d8f83938f9fdc5da2aa495d085907ad
Erik Magnusson <erik.magnusson@forefront.se>
2024-08-02 18:36:23 +0200

    Start of function that can store new users in DB

---
+++ API/AddUser.cs
@@ -0,0 +1,78 @@
+using System.Net;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace API
+{
+    public class TokenBody
+    {
+        public string? RefreshToken { get; set; }
+        public string? AuthorizationToken { get; set; }
+    }
+
+    public class User
+    {
+        [JsonPropertyName("id")]
+        public required string Id { get; set; }
+        [JsonPropertyName("userName")]
+        public string? UserName { get; set; }
+        [JsonPropertyName("refreshToken")]
+        public string? RefreshToken { get; set; }
+        [JsonPropertyName("accessToken")]
+        public string? AccessToken { get; set; }
+        [JsonPropertyName("tokenExpiresAt")]
+        public int TokenExpiresAt { get; set; }
+    }
+
+
+    public class AddUser
+    {
+        private readonly ILogger _logger;
+
+        public AddUser(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<AddUser>();
+        }
+
+        [OpenApiOperation(tags: ["User management"])]
+        [OpenApiParameter(name: "userId", In = ParameterLocation.Path)]
+        [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(TokenBody), Description = "Must provide either a refresh token or an authorization token")]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        [Function("AddUser")]
+        public async Task<ReturnType> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "{userId}/addUser")] HttpRequestData req, string userId)
+        {
+            var body = await req.ReadFromJsonAsync<TokenBody>();
+
+            var refreshToken = body.RefreshToken;
+
+            if (refreshToken == null)
+            {
+                if (body.AuthorizationToken == null)
+                    return new ReturnType{Result = Results.BadRequest("No tokens provided")};
+                
+                // TODO: Get refresh token from auth token
+                refreshToken = "Something non null";
+            }
+
+            if (refreshToken == null)
+                return new ReturnType{Result = Results.BadRequest("Auth token exchange did not provide refresh token")};
+
+            return new ReturnType{Result = Results.Ok("Stored user in database"), User = new User{Id = userId, RefreshToken = refreshToken}};
+        }
+
+        public class ReturnType
+        {
+            [HttpResult]
+            public IResult Result { get; set;}
+            [CosmosDBOutput("%CosmosDb%", "%UsersContainer%", Connection = "CosmosDBConnection", CreateIfNotExists = true, PartitionKey = "/id")]
+            public User? User { get; set;}
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults(worker => worker.UseNewtonsoftJson())
+    .ConfigureFunctionsWebApplication()
     .ConfigureOpenApi()
     .Build();
 
