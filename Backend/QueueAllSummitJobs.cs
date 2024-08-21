d7d1fa566c44eac914db045408c6d0089f2e6fd7
Erik Magnusson <erik.magnusson@forefront.se>
2024-08-21 16:27:48 +0200

    Queue summit jobs on new activities

---
+++ Backend/QueueAllSummitJobs.cs
@@ -0,0 +1,20 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Shared.Models;
+using Shared.Services;
+using static Backend.QueueActivityFetchJobs;
+
+namespace Backend
+{
+    public class QueueAllSummitJobs(CollectionClient<Activity> _cosmosClient)
+    {
+        [ServiceBusOutput("calculateSummitsJobs", Connection = "ServicebusConnection")]
+        [Function(nameof(QueueAllSummitJobs))]
+        public async Task<IEnumerable<CalculateSummitJob>> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestData req)
+        {
+            var ids = await _cosmosClient.GetAllIds();
+            return ids.Select(x => new CalculateSummitJob{ActivityId = x});
+        }
+    }
+}
--- Backend/QueueSummitJobs.cs
+++ Backend/QueueSummitJobs.cs
@@ -1,22 +1,25 @@
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
 using Shared.Models;
-using Shared.Services;
 
 namespace Backend
 {
-    public class QueueSummitJobs(CollectionClient<Activity> _cosmosClient)
+    public class QueueActivityFetchJobs()
     {
         public class CalculateSummitJob{
             public required string ActivityId { get; set; }
         }
 
         [ServiceBusOutput("calculateSummitsJobs", Connection = "ServicebusConnection")]
-        [Function("QueueSummitJobs")]
-        public async Task<IEnumerable<CalculateSummitJob>> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestData req)
+        [Function(nameof(QueueActivityFetchJobs))]
+        public IEnumerable<CalculateSummitJob> Run(
+            [CosmosDBTrigger(
+            databaseName: "%CosmosDb%",
+            containerName:"%ActivitiesContainer%",
+            Connection = "CosmosDBConnection",
+            LeaseContainerPrefix = "activitySummits",
+            CreateLeaseContainerIfNotExists = true)] IReadOnlyList<Activity> updatedActivities)
         {
-            var ids = await _cosmosClient.GetAllIds();
+            var ids = updatedActivities.Select(x => x.Id);
             return ids.Select(x => new CalculateSummitJob{ActivityId = x});
         }
     }
