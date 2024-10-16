4ae5729ba24ce8ce6608b9d947bb6629ed1fa5d4
Erik Magnusson <erik.magnusson@forefront.se>
2024-10-16 08:55:08 +0200

    Use bulk upsert in stravaActivitesFetcher. Had problem where it crashed on return and created too many jobs

--- Backend/StravaActivitiesFetcher.cs
+++ Backend/StravaActivitiesFetcher.cs
@@ -1,16 +1,17 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Shared.Models;
+using Shared.Services;
 using Shared.Services.StravaClient;
 
 namespace Backend
 {
-    public class StravaActivitiesFetcher(ILogger<StravaActivitiesFetcher> _logger, IHttpClientFactory httpClientFactory, ActivitiesApi _activitiesApi)
+    public class StravaActivitiesFetcher(ILogger<StravaActivitiesFetcher> _logger, IHttpClientFactory httpClientFactory, ActivitiesApi _activitiesApi, CollectionClient<Activity> _activitiesCollection)
     {
         readonly HttpClient _apiClient = httpClientFactory.CreateClient("backendApiClient");
-
+        [ServiceBusOutput("activitiesfetchjobs", Connection = "ServicebusConnection")]
         [Function(nameof(StravaActivitiesFetcher))]
-        public async Task<Outputs> Run([ServiceBusTrigger("activitiesfetchjobs", Connection = "ServicebusConnection")] ActivitiesFetchJob fetchJob)
+        public async Task<ActivitiesFetchJob?> Run([ServiceBusTrigger("activitiesfetchjobs", Connection = "ServicebusConnection")] ActivitiesFetchJob fetchJob)
         {
             var accessTokenResponse = await _apiClient.GetAsync($"{fetchJob.UserId}/accessToken");
             var accessToken = await accessTokenResponse.Content.ReadAsStringAsync();
@@ -19,27 +20,16 @@ namespace Backend
 
             var (activites, hasMorePages) = await _activitiesApi.GetActivitiesByAthlete(accessToken, page, fetchJob.Before, fetchJob.After);
 
-            var outputs = new Outputs()
-            {
-                WriteToActivities = activites.Select(ActivityMapper.MapSummaryActivity)
-            };
-
+            _logger.LogInformation("Fetched {amount} activities", activites?.Count() ?? 0);
+            if (activites != null)
+                await _activitiesCollection.BulkUpsert(activites.Select(ActivityMapper.MapSummaryActivity));
+                
             if (hasMorePages)
             {
                 fetchJob.Page = ++page;
-                outputs.NextPageJob = fetchJob;
+                return fetchJob;
             }
-            _logger.LogInformation("Fetched {amount} activities", activites?.Count() ?? 0);
-            outputs.WriteToActivities = activites.Select(ActivityMapper.MapSummaryActivity);
-            return outputs;
-        }
-
-        public class Outputs
-        {
-            [CosmosDBOutput("%CosmosDb%", "%ActivitiesContainer%", Connection = "CosmosDBConnection")]
-            public required IEnumerable<Activity> WriteToActivities { get; set; }
-            [ServiceBusOutput("activitiesfetchjobs", Connection = "ServicebusConnection")]
-            public ActivitiesFetchJob? NextPageJob { get; set; }
+            return default;
         }
     }
 }
